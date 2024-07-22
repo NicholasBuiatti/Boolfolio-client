88e0836fbe50b8008356a4de49cffee94667a487
NicholasBuiatti <nic.buiatti@gmail.com>
2024-07-22 19:42:41 +0200

    inserisco il link per il mio profilo git

--- src/components/routes/AppHome.vue
+++ src/components/routes/AppHome.vue
@@ -30,8 +30,7 @@ export default {
                     contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply
                     skip over it entirely.</p>
                 <p>
-                    <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                    <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+                    <a href="https://github.com/NicholasBuiatti" class="btn btn-primary my-2">Profilo Git</a>
                 </p>
             </div>
         </div>
