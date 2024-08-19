2c5ca228bc712a290dbebd5fdc5580a42add2945
NicholasBuiatti <nic.buiatti@gmail.com>
2024-08-19 14:29:57 +0200

    fix

--- src/components/routes/AppHome.vue
+++ src/components/routes/AppHome.vue
@@ -31,10 +31,9 @@ export default {
         <div class="row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
                 <h1 class="fw-light">Buiatti Nicholas</h1>
-                <p class="lead text-body-white">Something short and leading about the collection
-                    below—its
-                    contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply
-                    skip over it entirely.</p>
+                <p class="lead text-body-white">Portfolio di prova creato da un Jr Full Stack Web Developer per affinare
+                    le proprie capacita di relizzazione di un sito lato back-end e lato front-end con il ciclaggio su
+                    API create apposta e la gestione dei progetti da un sito accessibile solo dall'Admin.</p>
                 <p>
                     <a href="https://github.com/NicholasBuiatti" class="btn btn-primary my-2">Profilo Git</a>
                 </p>
@@ -43,16 +42,17 @@ export default {
     </section>
 
     <div class="album py-3 container">
-        <pre>{{ projects }}</pre>
+        <!-- <pre>{{ projects }}</pre> -->
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             <div v-for="project in projects" class="col">
                 <div class="card shadow-sm">
                     <img :src="project.img" alt="">
                     <div class="card-body">
-                        <p class="card-text">{{ project.name_project }}</p>
+                        <h4 class="card-text">{{ project.name_project }}</h4>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <router-link :to="{ name: 'Project', params: { slug: project.slug } }"
+                                    class="btn btn-sm btn-outline-secondary">Dettagli</router-link>
                             </div>
                             <small class="text-body-secondary">{{ project.date }}</small>
                         </div>
